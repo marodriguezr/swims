da245766b39d8b4c05f299e9bd284b67de7b6f1c
Miguel Rodríguez <miguelrodrii@outlook.com>
2022-04-24 20:29:37 -0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# MVN, JSF, Primefaces Boilerplate.
+
+Made possible by 
+1. [Create a Primefaces JSF project with Maven and Wildfly](https://medium.com/swlh/create-a-primefaces-jsf-project-with-maven-and-wildfly-bb695bed84c8)
+2. [rdebusscher](https://github.com/rdebusscher/jakartaEE9-started)
+3. [maven](https://mvnrepository.com/artifact/jakarta.platform/jakarta.jakartaee-web-api/8.0.0)
---
+++ war-module/src/main/java/hi/HelloBean.java
@@ -0,0 +1,56 @@
+package hi;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+import ejb2.HiManager;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+
+@Named("helloworld")
+@ViewScoped
+public class HelloBean implements Serializable {
+
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private HiManager hm;
+	
+	private int id;
+	private String name;
+
+public String getMessage() {
+      return "Hello World!";
+   }
+
+	@PostConstruct
+	public void onInit() {
+		id = -1;
+		name = "";
+	}
+	
+	public void actionListener() {
+		hm.add(0, "miguel");
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
