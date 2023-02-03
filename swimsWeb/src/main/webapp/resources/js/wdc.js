374255407944893a6dd3e3fea7754dc4d3c00e37
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-02-02 22:37:44 -0500

    modified standalone xml files

--- swimsWeb/src/main/webapp/resources/js/wdc.js
+++ swimsWeb/src/main/webapp/resources/js/wdc.js
@@ -1,62 +1,54 @@
-(function() {
-		    // Create the connector object
-		    var myConnector = tableau.makeConnector();
+(function () {
+  // Create the connector object
+  var myConnector = tableau.makeConnector();
 
-		    // Define the schema
-		    myConnector.getSchema = function(schemaCallback) {
-		        var cols = [{
-		            id: "id",
-		            dataType: tableau.dataTypeEnum.string
-		        }, {
-		            id: "mag",
-		            alias: "magnitude",
-		            dataType: tableau.dataTypeEnum.float
-		        }, {
-		            id: "title",
-		            alias: "title",
-		            dataType: tableau.dataTypeEnum.string
-		        }, {
-		            id: "location",
-		            dataType: tableau.dataTypeEnum.geometry
-		        }];
+  // Define the schema
+  myConnector.getSchema = function (schemaCallback) {
+    var cols = [
+      {
+        id: "url",
+        dataType: tableau.dataTypeEnum.string,
+      },
+    ];
 
-		        var tableSchema = {
-		            id: "earthquakeFeed",
-		            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-		            columns: cols
-		        };
+    var tableSchema = {
+      id: "earthquakeFeed",
+      alias:
+        "Earthquakes with magnitude greater than 4.5 in the last seven days",
+      columns: cols,
+    };
 
-		        schemaCallback([tableSchema]);
-		    };
+    schemaCallback([tableSchema]);
+  };
 
-		    // Download the data
-		    myConnector.getData = function(table, doneCallback) {
-		        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-		            var feat = resp.features,
-		                tableData = [];
+  // Download the data
+  myConnector.getData = function (table, doneCallback) {
+    $.getJSON(
+      "https://appfica.utn.edu.ec/swims/api/harvesting/thesis-records",
+      function (resp) {
+        console.log(resp);
+        var tableData = [];
 
-		            // Iterate over the JSON object
-		            for (var i = 0, len = feat.length; i < len; i++) {
-		                tableData.push({
-		                    "id": feat[i].id,
-		                    "mag": feat[i].properties.mag,
-		                    "title": feat[i].properties.title,
-		                    "location": feat[i].geometry
-		                });
-		            }
+        // Iterate over the JSON object
+        for (var i = 0, len = rest.length; i < len; i++) {
+          tableData.push({
+            url: resp[i].url,
+          });
+        }
 
-		            table.appendRows(tableData);
-		            doneCallback();
-		        });
-		    };
+        table.appendRows(tableData);
+        doneCallback();
+      }
+    );
+  };
 
-		    tableau.registerConnector(myConnector);
+  tableau.registerConnector(myConnector);
 
-		    // Create event listeners for when the user submits the form
-		    $(document).ready(function() {
-		        	$("#submitButton").click(function() {
-		            	tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-		            	tableau.submit(); // This sends the connector object to Tableau
-		        	});
-		    	});
-			})();
+  // Create event listeners for when the user submits the form
+  $(document).ready(function () {
+    $("#submitButton").click(function () {
+      tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+      tableau.submit(); // This sends the connector object to Tableau
+    });
+  });
+})();
