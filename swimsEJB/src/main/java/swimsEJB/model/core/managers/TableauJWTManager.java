74acdfe6af3380f8e9fef23915c79f7ea5d50606
Miguel Rodríguez <miguelrodrii@outlook.com>
2023-02-06 21:17:51 -0500

    Implemented process for automatic tableau jwt provisioning

--- swimsEJB/src/main/java/swimsEJB/model/core/managers/TableauJWTManager.java
+++ swimsEJB/src/main/java/swimsEJB/model/core/managers/TableauJWTManager.java
@@ -13,31 +13,36 @@ import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.*;
 import com.nimbusds.jwt.*;
 
+import static swimsEJB.constants.SystemEnvironmentVariables.TABLEAU_CLIENT_ID;
+import static swimsEJB.constants.SystemEnvironmentVariables.TABLEAU_USER;
+import static swimsEJB.constants.SystemEnvironmentVariables.TABLEAU_SECRET;
+import static swimsEJB.constants.SystemEnvironmentVariables.TABLEAU_SECRET_ID;
+
 @Stateless
 @LocalBean
 public class TableauJWTManager {
 
-    public TableauJWTManager() {
-    }
+	public TableauJWTManager() {
+	}
 
-    public String getTableauJWT() throws Exception {
-        String secret = "L5lr/0Z1UQQRSDro4IVNdMdNdFGqT5JaLHJPTzqsm+Y=";
-        String kid = "ae15681e-b733-4253-b58a-8510255d3a42";
-        String clientId = "bd21eb52-2a10-4043-8425-a1abe52602c8";
-        List<String> scopes = new ArrayList<>(Arrays.asList("tableau:views:embed"));
-        String username = "miguelrodrii@outlook.com";
-        JWSSigner signer = new MACSigner(secret);
-        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256).keyID(kid).customParam("iss", clientId).build();
-        JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .issuer(clientId).expirationTime(new Date(new Date().getTime() + 60 * 1000))
-                .jwtID(UUID.randomUUID().toString())
-                .audience("tableau")
-                .subject(username)
-                .claim("scp", scopes)
-                .build();
-        SignedJWT signedJWT = new SignedJWT(header, claimsSet);
-        signedJWT.sign(signer);
-        return signedJWT.serialize();
-        // model.addAttribute("token", signedJWT.serialize());
-    }
+	public String getTableauJWT() throws Exception {
+		if (TABLEAU_CLIENT_ID == null || TABLEAU_USER == null || TABLEAU_SECRET_ID == null || TABLEAU_SECRET == null)
+			throw new Exception("Variables de Tableau no registradas de forma adecuada.");
+		if (TABLEAU_CLIENT_ID.isBlank() || TABLEAU_USER.isBlank() || TABLEAU_SECRET_ID.isBlank()
+				|| TABLEAU_SECRET.isBlank())
+			throw new Exception("Variables de Tableau no registradas de forma adecuada.");
+
+		List<String> scopes = new ArrayList<>(Arrays.asList("tableau:views:embed"));
+		JWSSigner signer = new MACSigner(TABLEAU_SECRET);
+		JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256).keyID(TABLEAU_SECRET_ID)
+				.customParam("iss", TABLEAU_CLIENT_ID).build();
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder().issuer(TABLEAU_CLIENT_ID)
+				.expirationTime(new Date(new Date().getTime() + 60 * 1000)) // expires in 1 minute
+				.jwtID(UUID.randomUUID().toString()).audience("tableau").subject(TABLEAU_USER).claim("scp", scopes)
+				.build();
+		SignedJWT signedJWT = new SignedJWT(header, claimsSet);
+		signedJWT.sign(signer);
+
+		return signedJWT.serialize();
+	}
 }
--- swimsWeb/src/main/java/swimsWeb/controllers/core/TableauJWTBean.java
+++ swimsWeb/src/main/java/swimsWeb/controllers/core/TableauJWTBean.java
@@ -1,61 +1,42 @@
 package swimsWeb.controllers.core;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.view.ViewScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
-
 import swimsEJB.model.core.managers.TableauJWTManager;
-import swimsWeb.utilities.JSFMessages;
 
-@ViewScoped
+@SessionScoped
 @Named
 public class TableauJWTBean implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @EJB
-    private TableauJWTManager tableauJWTManager;
+	private static final long serialVersionUID = 1L;
+	@EJB
+	private TableauJWTManager tableauJWTManager;
+	private String tableauJWT;
 
-    public TableauJWTBean() {
-    }
+	public TableauJWTBean() {
+	}
+	
+	@PostConstruct
+	public void onLoad() {
+		try {
+			this.tableauJWT = tableauJWTManager.getTableauJWT();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    public String getTableauJWT() {
-        try {
-            String secret = "L5lr/0Z1UQQRSDro4IVNdMdNdFGqT5JaLHJPTzqsm+Y=";
-            String kid = "ae15681e-b733-4253-b58a-8510255d3a42";
-            String clientId = "bd21eb52-2a10-4043-8425-a1abe52602c8";
-            List<String> scopes = new ArrayList<>(Arrays.asList("tableau:views:embed"));
-            String username = "miguelrodrii@outlook.com";
-            JWSSigner signer = new MACSigner(secret);
-            JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.HS256).keyID(kid).customParam("iss", clientId)
-                    .build();
-            JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                    .issuer(clientId).expirationTime(new Date(new Date().getTime() + 60 * 1000))
-                    .jwtID(UUID.randomUUID().toString())
-                    .audience("tableau")
-                    .subject(username)
-                    .claim("scp", scopes)
-                    .build();
-            SignedJWT signedJWT = new SignedJWT(header, claimsSet);
-            signedJWT.sign(signer);
-            return signedJWT.serialize();
-        } catch (Exception e) {
-            // TODO: handle exception
-            JSFMessages.ERROR("Error en la generación del Token de Tableau");
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public String getTableauJWT() {
+		return tableauJWT;
+	}
+
+	public void setTableauJWT(String tableauJWT) {
+		this.tableauJWT = tableauJWT;
+	}
+
+	
 }
